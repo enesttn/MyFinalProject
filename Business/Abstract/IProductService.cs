9a5799959b6ed77423a2819dc2b55e88abbe3d0a
Yusuf Enes Tütün <enestutun@hotmail.com>
2023-11-20 12:52:00 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System.Collections.Generic;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        IDataResult<List<Product>> GetAll();      //IEntity katmanındaki gibi GetAll listesi oluşturuyoruz.
+        IDataResult<List<Product>> GetAllCategoryId(int id);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IDataResult<Product> GetById(int productId);
+        IResult Add(Product product);
+    }
+}
