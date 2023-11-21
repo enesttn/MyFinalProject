567699dccbe37e0dc627dae96f31b75927d198ef
enesttn <83828133+enesttn@users.noreply.github.com>
2023-11-21 21:02:07 +0300

    IProductService.cs

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -7,7 +7,7 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
-        IDataResult<List<Product>> GetAll();      //IEntity katmanındaki gibi GetAll listesi oluşturuyoruz.
+        IDataResult<List<Product>> GetAll();      
         IDataResult<List<Product>> GetAllCategoryId(int id);
         IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
         IDataResult<List<ProductDetailDto>> GetProductDetails();
